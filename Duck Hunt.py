5403e6fbf30c1176fc48c0b5a37a7d0658793b55
Ivan Zadorozhniy <vakikrin@gmail.com>
2018-10-16 00:03:38 +0300

    Add a jump and landing for Dog. And fix animation jump

--- Duck Hunt.py
+++ Duck Hunt.py
@@ -1,22 +1,40 @@
 from utils import *
 
-bg = pygame.image.load("images//background.png")
-bg = pygame.transform.scale(bg,screen.get_size())
-screen.blit(bg, (0, 0))
-dog = Dog(BLACK, 0, 360)
+bg = pygame.image.load("images//background.png").convert()
+bg = pygame.transform.scale(bg, screen.get_size())
+bg.set_colorkey(bg.get_at((0,0)))
+
+
+# screen.fill((61, 191, 255))
+
+disp = pygame.Surface(screen.get_size())
+
+
+dog = Dog(0, 360, 100, 100)
 dogs = pygame.sprite.Group()
 dogs.add(dog)
+
 running = True
 clock = pygame.time.Clock()
 
-
 while running:
-    screen.blit(bg, (0, 0))
+    disp.fill((61, 191, 255))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     dogs.update()
-    dogs.draw(screen)
+    if dog.getStage() == "Landing":
+
+        dogs.draw(disp)
+        disp.blit(bg, (0, 0))
+    else:
+        disp.blit(bg, (0, 0))
+        dogs.draw(disp)
+    screen.blit(disp, (0, 0))
+
     pygame.display.update()
     clock.tick(30)
 pygame.quit()
+
+
--- dog.py
+++ dog.py
@@ -1,22 +1,72 @@
 from settings import *
-images = [('images//move'+str(num)+".png" , 0.2) for num in range(1,5)]
-dogAnimation = PygAnimation(images)
+
+dogMoveImg = [('images//move' + str(num) + ".png", 0.2) for num in range(1, 5)]
+dogMoveAnimation = PygAnimation(dogMoveImg)
+dogJumpImg = [('images//jump' + str(num) + ".png", 0.1) for num in range(1, 3)]
+print(len(dogJumpImg))
+dogJumpAnimation = PygAnimation([dogJumpImg[0]])
+dogLandingAnimation = PygAnimation([dogJumpImg[1]])
+
 class Dog(pygame.sprite.Sprite):
-    def __init__( self, color, x, y):
-        super( Dog, self ).__init__()
-        self.animation = dogAnimation
-        self.animation.scale((100,100))
-        self.size = self.animation.getRect()
-        self.image = pygame.Surface((100, 100))
-        self.image.fill((100,100,100))
-        self.image.set_colorkey((100,100,100))
+    def __init__(self, x, y, width, height):
+        super(Dog, self).__init__()
+
+        self.animation = dogMoveAnimation
+        self.size = (width, height)
+        self.animation.scale(self.size)
+
+
+
+        self.image = pygame.Surface(self.size)
+        self.image.fill((100, 100, 100))
+        self.image.set_colorkey((100, 100, 100))
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
+        self.stages = ["Move","Jump","Landing"]
+        self.stage = self.stages[0]
         self.animation.play()
+
     def update(self):
-        print(self.size)
-        self.image.fill((100,100,100))
-        self.animation.blit(self.image,(0,0))
-        self.rect.x += 2
+        if self.animation == dogMoveAnimation:
+            if self.rect.x < 250:
+                self.move()
+            else:
+                self.animation.stop()
+                self.animation = dogJumpAnimation
+                self.animation.scale(self.size)
+                self.animation.play()
+                self.stage = self.stages[1]
+        elif self.animation == dogJumpAnimation:
+            if self.rect.y > 280:
+                self.jump()
+            else:
+                self.animation.stop()
+                self.animation = dogLandingAnimation
+                self.animation.scale(self.size)
+                self.animation.play()
+                self.stage = self.stages[2]
+        elif self.animation == dogLandingAnimation:
+            if self.rect.y < 400:
+                self.landing()
+            else:
+                self.animation.stop()
+
+
+    def move(self):
+        self.image.fill((100, 100, 100))
+        self.animation.blit(self.image, (0, 0))
+        self.rect.x += 10
+    def jump(self):
+        self.image.fill((100, 100, 100))
+        self.animation.blit(self.image, (0, 0))
+        self.rect.x += 5
+        self.rect.y -= 10
+    def landing(self):
+        self.image.fill((100, 100, 100))
+        self.animation.blit(self.image, (0, 0))
+        self.rect.x += 1
+        self.rect.y += 10
+    def getStage(self):
+        return self.stage
